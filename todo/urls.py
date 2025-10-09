6bba8c21cdf81e9ab41e7b997c66b90a9b0ea802
Bhairab-Nath <nathbhairab22@gmail.com>
2025-10-09 15:10:31 +0545

    Update, Delete task done

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #Add task feature
     path('addTask/', views.addTask, name= 'addTask'),
+
+    #Mark as Done feature
+    path('markAsDone/<int:pk>/', views.markAsDone, name='markAsDone'),
+
+    #Undo feature
+    path('undoTask/<int:pk>/', views.undoTask, name='undoTask'),
+
+    #Edit feature
+    path('editTask/<int:pk>/', views.editTask, name='editTask'),
+
+    #Delete feature
+    path('deleteTask/<int:pk>/', views.deleteTask, name='deleteTask'),
+    
 ]
 
 
